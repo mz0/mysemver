b2160a4a69c965928b86b77a8d005c29ae3c2118
MarcuZ <mz@bemigot.net>
2018-08-02 18:26:14 +0300

    remove disfunctional properties reading

--- src/main/java/com/exactpro/MyVer.java
+++ src/main/java/com/exactpro/MyVer.java
@@ -1,29 +1,11 @@
 package com.exactpro;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 public class MyVer {
     public static void main(String[] args) {
-        String verFile = "version.properties";
-        Properties version = new Properties();
-        try {
-            FileInputStream f = new FileInputStream(verFile);
-            version.load(f);
-            f.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(String.format("Version file '%s' not found!",verFile));
-        } catch (IOException e) {
-            System.out.println(String.format("Error reading version file '%s': %s",verFile, e.getMessage()));
-        }
-
-        String ver=version.getProperty("version");
-        System.out.println(String.format("Hello, this is build %s of MyVer project according to %s",ver,verFile));
-
         Manifest mf = new Manifest();
         try {
           mf.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/MANIFEST.MF"));
